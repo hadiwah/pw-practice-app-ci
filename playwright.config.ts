1015a370ff194cc34962defca3c2121d07c3ce9b
hadiwah <wahyuhadi1296@gmail.com>
2026-01-15 18:20:49 +0700

    update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,16 +19,8 @@ export default defineConfig<TestOptions>({
     timeout: 2000,
     toMatchSnapshot: {maxDiffPixels: 50}
   },
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  
+  retries: 1,
   reporter: [
     ['json', {outputFile: 'test-results/jsonReport.json'}],
     ['junit', {outputFile: 'test-results/junitReport.xml'}],
